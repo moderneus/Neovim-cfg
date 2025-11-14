3156ac9f59873a20e71a3385b30dde465dc60c03
moderneus <finelefrinchik@gmail.com>
2025-11-14 12:28:10 +0400

    New theme

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000,
+    config = function()
+      require("onedarkpro").setup {
+        colors = {
+          bg = "#08090d", -- тёмно-синий фон
+          fg = "#c5c8c9",
+          blue = "#61afef",
+          cyan = "#56b6c2",
+          red = "#ffffff",
+          yellow = "#e5c07b",
+          orange = "#d19a66",
+        },
+        options = {
+          transparency = false,
+          terminal_colors = false,
+        },
+      }
+      vim.cmd("colorscheme onedark_dark")
+    end,
+  },
+}
