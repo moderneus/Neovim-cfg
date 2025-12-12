dc10006f70eabcfab96b4087c15f61b5f5d9e8dc
moderneus <moderneusf@gmail.com>
2025-12-13 00:50:00 +0400

    smooth cursor

---
+++ lua/plugins/cursor.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "sphamba/smear-cursor.nvim",
+    lazy = false,
+    opts = {
+      smear_between_buffers = true,
+      smear_between_windows = true,
+      length = 50,
+      elastic = true,
+    },
+  },
+}
